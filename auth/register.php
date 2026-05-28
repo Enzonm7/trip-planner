a4b181922943ef5ca1cde6104a9bbd16f55debae
Leo Degrugillier <leo.degrugillier2205@gmail.com>
2026-05-28 14:53:48 +0200

    feat(auth): add complete register and logout flows

---
+++ auth/register.php
@@ -0,0 +1,67 @@
+<?php
+session_start();
+require("../config/db.php");
+
+if ($_SERVER["REQUEST_METHOD"]=="POST"){
+    if ($_POST['action']=="register"){
+        $username=$_POST["username"];
+        $email=$_POST["email"];
+        $pswd=$_POST["pswd"];
+
+        if ($username=="" || $email=="" || $pswd==""){
+            $error1="Please complete all informations";
+            echo $error1;
+        }
+        elseif(!str_contains($email,"@")){
+            $error2="Your email is incorrect";
+            echo $error2;
+        }
+            
+        else{
+            $pswd_hashed=password_hash($pswd,PASSWORD_DEFAULT);
+            $req=$pdo->prepare("INSERT INTO users(username,email,password_hash) VALUES (?,?,?);");
+            $req->execute([$username,$email,$pswd_hashed]);
+            $result="Account Created";
+            echo $result;
+        }
+    }
+    if ($_POST['action']=="back"){
+        header("Location: ../index.php");
+        exit();
+    }
+    if ($_POST['action']=='login'){
+        header("Location: login.php");
+        exit();
+    }
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+    <h1>Account Creation</h1>
+    <form action="" method="POST">
+        <input type="hidden" name="action" value="register">
+        <input type="text" placeholder="Email" name="email">
+        <input type="text" placeholder="Username" name="username">
+        <input type="password" placeholder="Password" name="pswd">
+        <button type="submit">Create</button>
+    </form>
+    <form action="" method="POST">
+        <input type="hidden" name="action" value="back">
+        <button type="submit">Back</button>
+    </form>
+
+    <form action="" method="POST">
+        <input type="hidden" name="action" value="login">
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
